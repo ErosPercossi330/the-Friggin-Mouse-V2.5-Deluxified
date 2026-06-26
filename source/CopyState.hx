929bc513deb710496249925b63afa4048979f248
Ethantobot <ethantobot7@gmail.com>
2026-06-26 20:50:24 +0200

    d

--- source/CopyState.hx
+++ source/CopyState.hx
@@ -1,211 +1,270 @@
-package mobile.states;
+/*
+ * Copyright (C) 2025 Mobile Porting Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
+package states;
+
+#if COPYSTATE_ALLOWED
+import states.TitleState;
 import lime.utils.Assets as LimeAssets;
 import openfl.utils.Assets as OpenFLAssets;
-import flixel.addons.util.FlxAsyncLoop;
 import openfl.utils.ByteArray;
 import haxe.io.Path;
 import flixel.ui.FlxBar;
 import flixel.ui.FlxBar.FlxBarFillDirection;
-import sys.FileSystem;
-import sys.io.File;
-import haxe.crypto.Md5;
-import openfl.system.System;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
+import lime.system.ThreadPool;
 
-/*
- * Optimized CopyState with Content Validation and RAM Throttling
+/**
+ * ...
+ * @author: Karim Akra
  */
 class CopyState extends MusicBeatState
 {
-    private static final textFilesExtensions:Array<String> = ['ini', 'txt', 'xml', 'hxs', 'hx', 'lua', 'json', 'frag', 'vert'];
-    public static final IGNORE_FOLDER_FILE_NAME:String = "ignore.txt";
-    private static var directoriesToIgnore:Array<String> = [];
-    public static var locatedFiles:Array<String> = [];
-    public static var maxLoopTimes:Int = 0;
+	private static final textFilesExtensions:Array<String> = ['ini', 'txt', 'xml', 'hxs', 'hx', 'lua', 'json', 'frag', 'vert'];
+	public static final IGNORE_FOLDER_FILE_NAME:String = "CopyState-Ignore.txt";
+	private static var directoriesToIgnore:Array<String> = [];
+	public static var locatedFiles:Array<String> = [];
+	public static var maxLoopTimes:Int = 0;
 
-    public var loadingImage:FlxSprite;
-    public var loadingBar:FlxBar;
-    public var loadedText:FlxText;
-    public var copyLoop:FlxAsyncLoop;
+	public var loadingImage:FlxSprite;
+	public var loadingBar:FlxBar;
+	public var loadedText:FlxText;
+	public var thread:ThreadPool;
 
-    private var _isPaused:Bool = false;
-    var failedFilesStack:Array<String> = [];
-    var failedFiles:Array<String> = [];
-    var shouldCopy:Bool = false;
-    var canUpdate:Bool = true;
-    var loopTimes:Int = 0; 
-    var ramLimit:Float = 1024 * 1024 * 700;
+	var failedFilesStack:Array<String> = [];
+	var failedFiles:Array<String> = [];
+	var shouldCopy:Bool = false;
+	var canUpdate:Bool = true;
+	var loopTimes:Int = 0;
 
-    override function create()
-    {    
-        if (maxLoopTimes <= 0)
-        {
-            MusicBeatState.switchState(new TitleState());
-            return;
-        }
+	override function create()
+	{
+		locatedFiles = [];
+		maxLoopTimes = 0;
+		checkExistingFiles();
+		if (maxLoopTimes <= 0)
+		{
+			MusicBeatState.switchState(new TitleState());
+			return;
+		}
 
-        shouldCopy = true;
+		CoolUtil.showPopUp("Seems like you have some missing files that are necessary to run the game\nPress OK to begin the copy process", "Notice!");
 
-        add(new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, 0xffcaff4d));
+		shouldCopy = true;
 
-        loadingImage = new FlxSprite(0, 0, Paths.image('funkay'));
-        loadingImage.setGraphicSize(0, FlxG.height);
-        loadingImage.updateHitbox();
-        loadingImage.screenCenter();
-        add(loadingImage);
+		add(new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, 0xffcaff4d));
 
-        loadingBar = new FlxBar(0, FlxG.height - 26, FlxBarFillDirection.LEFT_TO_RIGHT, FlxG.width, 26);
-        loadingBar.setRange(0, maxLoopTimes);
-        add(loadingBar);
+		loadingImage = new FlxSprite(0, 0, Paths.image('funkay'));
+		loadingImage.setGraphicSize(0, FlxG.height);
+		loadingImage.updateHitbox();
+		loadingImage.screenCenter();
+		add(loadingImage);
 
-        loadedText = new FlxText(loadingBar.x, loadingBar.y + 4, FlxG.width, '', 16);
-        loadedText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, CENTER);
-        add(loadedText);
+		loadingBar = new FlxBar(0, FlxG.height - 26, FlxBarFillDirection.LEFT_TO_RIGHT, FlxG.width, 26);
+		loadingBar.setRange(0, maxLoopTimes);
+		add(loadingBar);
 
-        copyLoop = new FlxAsyncLoop(maxLoopTimes, copyAsset, 4);
-        add(copyLoop);
-        copyLoop.start();
+		loadedText = new FlxText(loadingBar.x, loadingBar.y + 4, FlxG.width, '', 16);
+		loadedText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, CENTER);
+		add(loadedText);
 
-        super.create();
-    }
+		thread = new ThreadPool(0, CoolUtil.getCPUThreadsCount());
+		thread.doWork.add(function(poop)
+		{
+			for (file in locatedFiles)
+			{
+				loopTimes++;
+				copyAsset(file);
+			}
+		});
+		new FlxTimer().start(0.5, (tmr) ->
+		{
+			thread.queue({});
+		});
 
-    override function update(elapsed:Float)
-    {
-        if (shouldCopy && copyLoop != null)
-        {
-            var currentMemory:Float = System.totalMemory;
-            if (currentMemory > ramLimit || FlxG.drawFramerate < 20) {
-                _isPaused = true;
-            } else {
-                _isPaused = false;
-            }
+		super.create();
+	}
 
-            loadingBar.value = loopTimes;
-            
-            if (copyLoop.finished && canUpdate)
-            {
-                canUpdate = false;
-                FlxG.sound.play(Paths.sound('confirmMenu')).onComplete = () -> {
-                    MusicBeatState.switchState(new TitleState());
-                };
-            }
+	override function update(elapsed:Float)
+	{
+		if (shouldCopy)
+		{
+			if (loopTimes >= maxLoopTimes && canUpdate)
+			{
+				if (failedFiles.length > 0)
+				{
+					CoolUtil.showPopUp(failedFiles.join('\n'), 'Failed To Copy ${failedFiles.length} File.');
+					final folder:String = #if android SUtil.getExternalStorageDirectory() + #else Sys.getCwd() + #end 'logs/';
+					if (!FileSystem.exists(folder))
+						FileSystem.createDirectory(folder);
+					File.saveContent(folder + Date.now().toString().replace(' ', '-').replace(':', "'") + '-CopyState' + '.txt', failedFilesStack.join('\n'));
+				}
+				
+				FlxG.sound.play(Paths.sound('confirmMenu')).onComplete = () ->
+				{
+					MusicBeatState.switchState(new TitleState());
+				};
+		
+				canUpdate = false;
+			}
 
-            loadedText.text = (_isPaused ? "RAM LIMIT REACHED... " : "Updating Assets: ") + '$loopTimes/$maxLoopTimes';
-        }
-        super.update(elapsed);
-    }
+			if (loopTimes >= maxLoopTimes)
+				loadedText.text = "Completed!";
+			else
+				loadedText.text = '$loopTimes/$maxLoopTimes';
 
-    public function copyAsset()
-    {
-        if (_isPaused) return;
+			loadingBar.percent = Math.min((loopTimes / maxLoopTimes) * 100, 100);
+		}
+		super.update(elapsed);
+	}
 
-        var file = locatedFiles[loopTimes];
-        loopTimes++;
-        
-        var internalPath = getFile(file);
-        var shouldWrite = false;
+	public function copyAsset(file:String)
+	{
+		if (!FileSystem.exists(file))
+		{
+			var directory = Path.directory(file);
+			if (!FileSystem.exists(directory))
+				FileSystem.createDirectory(directory);
+			try
+			{
+				if (OpenFLAssets.exists(getFile(file)))
+				{
+					if (textFilesExtensions.contains(Path.extension(file)))
+						createContentFromInternal(file);
+					else
+					{
+						var path:String = '';
+						#if android
+						if (file.startsWith('mods/'))
+							path = SUtil.getExternalStorageDirectory() + file;
+						else
+						#end
+							path = file;
+						File.saveBytes(path, getFileBytes(getFile(file)));
+					}		
+				}
+				else
+				{
+					failedFiles.push(getFile(file) + " (File Dosen't Exist)");
+					failedFilesStack.push('Asset ${getFile(file)} does not exist.');
+				}
+			}
+			catch (e:haxe.Exception)
+			{
+				failedFiles.push('${getFile(file)} (${e.message})');
+				failedFilesStack.push('${getFile(file)} (${e.stack})');
+			}
+		}
+	}
 
-        if (!FileSystem.exists(file)) {
-            shouldWrite = true;
-        } else {
-            try {
-                var internalBytes = OpenFLAssets.getBytes(internalPath);
-                var externalBytes = File.getBytes(file);
-                if (Md5.encode(internalBytes.toString()) != Md5.encode(externalBytes.toString())) {
-                    shouldWrite = true;
-                }
-            } catch(e) { shouldWrite = true; }
-        }
+	public function createContentFromInternal(file:String)
+	{
+		var fileName = Path.withoutDirectory(file);
+		var directory = Path.directory(file);
+		#if android
+		if (fileName.startsWith('mods/'))
+			directory = SUtil.getExternalStorageDirectory() + directory;
+		#end
+		try
+		{
+			var fileData:String = OpenFLAssets.getText(getFile(file));
+			if (fileData == null)
+				fileData = '';
+			if (!FileSystem.exists(directory))
+				FileSystem.createDirectory(directory);
+			File.saveContent(Path.join([directory, fileName]), fileData);
+		}
+		catch (e:haxe.Exception)
+		{
+			failedFiles.push('${getFile(file)} (${e.message})');
+			failedFilesStack.push('${getFile(file)} (${e.stack})');
+		}
+	}
 
-        if (shouldWrite)
-        {
-            var directory = Path.directory(file);
-            if (!FileSystem.exists(directory))
-                StorageUtil.createDirectories(directory);
-            
-            try {
-                if (textFilesExtensions.contains(Path.extension(file)))
-                    createContentFromInternal(file);
-                else
-                    File.saveBytes(file, getFileBytes(internalPath));
-            } catch (e:haxe.Exception) {
-                failedFiles.push('${file} (Error: ${e.message})');
-            }
-        }
-    }
+	public function getFileBytes(file:String):ByteArray
+	{
+		switch (Path.extension(file).toLowerCase())
+		{
+			case 'otf' | 'ttf':
+				return ByteArray.fromFile(file);
+			default:
+				return OpenFLAssets.getBytes(file);
+		}
+	}
 
-    public static function checkExistingFiles():Bool
-    {
-        locatedFiles = [];
-        directoriesToIgnore = [];
-        
-        var fullList:Array<String> = OpenFLAssets.list();
-        var assets = fullList.filter(f -> f.startsWith('assets/') || f.startsWith('mods/') || f.startsWith('modpack/'));
-        
-        for (file in assets)
-        {
-            if (file.endsWith('/') || OpenFLAssets.isLocal(file, "DIR")) continue;
+	public static function getFile(file:String):String
+	{
+		if (OpenFLAssets.exists(file))
+			return file;
 
-            if (file.endsWith(IGNORE_FOLDER_FILE_NAME))
-                directoriesToIgnore.push(Path.directory(file));
-            
-            var skip = false;
-            for (dir in directoriesToIgnore) {
-                if (file.startsWith(dir)) skip = true;
-            }
-            if (skip) continue;
+		@:privateAccess
+		for (library in LimeAssets.libraries.keys())
+		{
+			if (OpenFLAssets.exists('$library:$file') && library != 'default')
+				return '$library:$file';
+		}
 
-            if (FileSystem.exists(file)) {
-                try {
-                    var info = FileSystem.stat(file);
-                    @:privateAccess
-                    var internalSize:Int = 0;
-                    var assetPath = getFile(file);
-                    
-                    if (LimeAssets.exists(assetPath)) {
-                        var libraryName = assetPath.contains(":") ? assetPath.split(":")[0] : "default";
-                        var symbolID = assetPath.contains(":") ? assetPath.split(":")[1] : assetPath;
-                        var library = LimeAssets.getLibrary(libraryName);
-                        
-                        if (library != null) {
-                            internalSize = library.exists(symbolID, "BINARY") ? library.size(symbolID) : -1;
-                        }
-                    }
-                    if (internalSize != -1 && info.size == internalSize) {
-                        continue;
-                    }
-                } catch(e:Dynamic) {
-                }
-            }
+		return file;
+	}
 
-            locatedFiles.push(file);
-        }
+	public static function checkExistingFiles():Bool
+	{
+		locatedFiles = OpenFLAssets.list();
 
-        maxLoopTimes = locatedFiles.length;
-        return (maxLoopTimes <= 0);
-    }
+		// removes unwanted assets
+		var assets = locatedFiles.filter(folder -> folder.startsWith('assets/'));
+		var mods = locatedFiles.filter(folder -> folder.startsWith('mods/'));
+		locatedFiles = assets.concat(mods);
+		locatedFiles = locatedFiles.filter(file -> !FileSystem.exists(file));
+		#if android
+		for (file in locatedFiles)
+			if (file.startsWith('mods/'))
+				locatedFiles = locatedFiles.filter(file -> !FileSystem.exists(SUtil.getExternalStorageDirectory() + file));
+		#end
 
-    public function createContentFromInternal(file:String) {
-        try {
-            var fileData:String = OpenFLAssets.getText(getFile(file));
-            File.saveContent(file, fileData == null ? '' : fileData);
-        } catch (e:haxe.Exception) {}
-    }
+		var filesToRemove:Array<String> = [];
 
-    public function getFileBytes(file:String):ByteArray {
-        var extension = Path.extension(file).toLowerCase();
-        return (extension == 'ttf' || extension == 'otf') ? 
-            ByteArray.fromFile(file) : OpenFLAssets.getBytes(file);
-    }
+		for (file in locatedFiles)
+		{
+			if (filesToRemove.contains(file))
+				continue;
 
-    public static function getFile(file:String):String {
-        if (OpenFLAssets.exists(file)) return file;
-        @:privateAccess
-        for (library in LimeAssets.libraries.keys()) {
-            if (OpenFLAssets.exists('$library:$file') && library != 'default') return '$library:$file';
-        }
-        return file;
-    }
+			if(file.endsWith(IGNORE_FOLDER_FILE_NAME) && !directoriesToIgnore.contains(Path.directory(file)))
+				directoriesToIgnore.push(Path.directory(file));
+
+			if (directoriesToIgnore.length > 0)
+			{
+				for (directory in directoriesToIgnore)
+				{
+					if (file.startsWith(directory))
+						filesToRemove.push(file);
+				}
+			}
+		}
+
+		locatedFiles = locatedFiles.filter(file -> !filesToRemove.contains(file));
+
+		maxLoopTimes = locatedFiles.length;
+
+		return (maxLoopTimes <= 0);
+	}
 }
+#end
