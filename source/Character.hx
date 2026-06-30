f4f84e9c6963f3b901a538efb9fcdb21305d30f6
Ethantobot <ethantobot7@gmail.com>
2026-06-30 08:32:12 +0200

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -107,7 +107,7 @@ class Character extends FlxSprite
 				if (!Assets.exists(path))
 				#end
 				{
-					path = SUtil.getStorageDirectory() + Paths.getPreloadPath('characters/' + DEFAULT_CHARACTER + '.json'); //If a character couldn't be found, change him to BF just to prevent a crash
+					path = Paths.getPreloadPath('characters/' + DEFAULT_CHARACTER + '.json'); //If a character couldn't be found, change him to BF just to prevent a crash
 				}
 
 				#if MODS_ALLOWED
