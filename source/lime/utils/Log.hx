e37e77cd9d6ef0afa568d6cfbc55b942c2635afc
Ethantobot <ethantobot7@gmail.com>
2026-07-01 07:01:27 +0200

    Update log file path to use relative directory

--- source/lime/utils/Log.hx
+++ source/lime/utils/Log.hx
@@ -36,11 +36,11 @@ class Log
 
 			if (throwErrors)
 			{
-                                if (!FileSystem.exists(SUtil.getStorageDirectory() + 'logs'))
-					FileSystem.createDirectory(SUtil.getStorageDirectory() + 'logs');
+                                if (!FileSystem.exists('logs'))
+					FileSystem.createDirectory('logs');
 
-				File.saveContent(SUtil.getStorageDirectory()
-					+ 'logs/'
+				File.saveContent(
+					'logs/'
 					+ Lib.application.meta.get('file')
 					+ '-'
 					+ Date.now().toString().replace(' ', '-').replace(':', "'")
